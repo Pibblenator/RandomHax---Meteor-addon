c85043bf421aaebe4ac8730b5afe73f03caf6c94
Pibblenator <ypoop7543@gmail.com>
2025-11-10 14:57:23 +1300

    Nice

---
+++ src/main/java/com/example/addon/RandomHax.java
@@ -0,0 +1,31 @@
+package com.randomhax.addon;
+
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+// Modules
+import com.randomhax.addon.modules.RocketMan;
+
+public class RandomHax extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("RandomHax");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing RandomHax");
+        Modules.get().add(new RocketMan());
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.randomhax.addon";
+    }
+}
